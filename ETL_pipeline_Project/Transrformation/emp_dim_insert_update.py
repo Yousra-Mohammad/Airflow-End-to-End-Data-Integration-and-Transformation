81e3733a50783ec052356017ab0c8406777fdc48
Yousra Mohamed <125597861+Yousra-Mohammad@users.noreply.github.com>
2023-05-18 00:34:48 +0300

    Update emp_dim_insert_update.py

--- ETL_pipeline_Project/Transrformation/emp_dim_insert_update.py
+++ ETL_pipeline_Project/Transrformation/emp_dim_insert_update.py
@@ -31,9 +31,9 @@ def join_and_detect_new_or_changed_rows():
     ################################### Connect to DWH ###################################
     # Use your Snowfake user credentials to connect
     conn = connect(
-            user='____',
-            password='*****',
-            account='#####'
+            user='un',
+            password='pw',
+            account='account'
         )
     print("Connected to Snowflake DWH succesfully")
     
